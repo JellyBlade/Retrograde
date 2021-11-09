25e58272a3ef023acb26c479e69f7c98498084ab
Tyler Hippard <tyler.hippard@uleth.ca>
2021-11-08 22:46:19 -0700

    Created and documented NamedThing.h

---
+++ include/NamedThing.h
@@ -0,0 +1,46 @@
+#ifndef NAMED_THING_H
+#define NAMED_THING_H
+
+#include <string>
+
+/**
+* Provides implementation for things that have names and descriptions, such
+* as rooms, NPCs, and all objects.
+*/
+class NamedThing {
+public:
+  /**
+  * Constructor
+  */
+  NamedThing(string name, string description);
+
+  /**
+  * Returns this thing's name.
+  * @return a string containing this thing's name.
+  */
+  string getName()
+
+  /**
+  * Changes this thing's name to the given string.
+  * @param newName the name to change to.
+  */
+  void setName(string newName);
+
+  /**
+  * Returns this thing's description.
+  * @return a string describing this thing.
+  */
+  string getDescription();
+
+  /**
+  * Changes this thing's description to the given string.
+  * @param newDescription the description to change to.
+  */
+  void setDescription(string newDescription);
+  
+private:
+  string name;
+  string description;
+}
+
+#endif // NAMED_THING_H
