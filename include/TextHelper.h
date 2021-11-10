c8ad1d51ea41941398d5acf749625f21caed501b
Tyler Hippard <tyler.hippard@uleth.ca>
2021-11-10 14:59:03 -0700

    Implemented TextHelper tolower, toupper, trim, and trimAll.

--- include/TextHelper.h
+++ include/TextHelper.h
@@ -20,42 +20,42 @@ namespace TextHelper {
   * calling commandProcessor if it encounters a script command to execute.
   * @param textPath to the path of the file to read from.
   */
-  void readFile(string textPath);
+  void readFile(std::string textPath);
 
   /**
   * Performs functions based on the inputted command string, usually read
   * from a dialogue file.
   * @param command the script command to parse and execute.
   */
-  void commandProcessor(string command);
+  void commandProcessor(std::string command);
 
   /**
   * Replaces all characters in the string with their lowercase counterparts.
   * @param s the string to turn into lowercase
   * @return a lowercase version of the input string.
   */
-  string tolower(string s);
+  std::string tolower(std::string s);
 
   /**
   * Replaces all characters in the string with their uppercase counterparts.
   * @param s the string to turn into uppercase.
   * @return an uppercase version of the input string.
   */
-  string toupper(string s);
+  std::string toupper(std::string s);
 
   /**
   * Removes leading/trailing whitespace from the string.
   * @param s the string to trim.
   * @return a string without whitespace at the beginning or end.
   */
-  string trim(string s);
+  std::string trim(std::string s);
 
   /**
   * Removes all whitespace from the string.
   * @param s the string to remove all whitespace from.
   * @return a string without any whitespace.
   */
-  string trimAll(string s);
+  std::string trimAll(std::string s);
 
 } // namespace TextHelper
 
