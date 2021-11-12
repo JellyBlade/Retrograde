a97273b995cf049f0b4cf232b6430ab84e5eff0e
Tyrell Martens <tyrell.martens@uleth.ca>
2021-11-12 12:22:41 -0700

    No more couts and style changes

--- src/Map.cpp
+++ src/Map.cpp
@@ -13,21 +13,14 @@
 //TODO: Should this funciton and setAllRoomOxygen throw exceptions if there are
 // no elements in the vectors?
 Room* Map::getRoom(std::string n) {
-  std::cout << "Does it even get here?" << std::endl;
-  if(this->rooms.size() < 1) {
-    std::cout << "Return nullptr" << std::endl;
+  if (this->rooms.size() < 1)
     return nullptr;
-  }
-  std::cout << "Right before the for loop" << std::endl;
-  for(int i = 0; i < rooms.size(); i++) {
-    std::cout << "in the for loop" << std::endl;
+  for (int i = 0; i < rooms.size(); i++) {
     if (TextHelper::tolower(TextHelper::trimAll(rooms.at(i)->getName())) ==
     TextHelper::tolower(TextHelper::trimAll(n))) {
-    std::cout << "before return rooms.at(i)" << std::endl;
       return rooms.at(i);
     }
   }
-  std::cout << "Returns the last nullptr" << std::endl;
   return nullptr;
 }
 
@@ -48,7 +41,7 @@ void Map::setAllRoomOxygen(double oxygen) {
   if(rooms.size() == 0)
     throw exception
   */
-  for(Room* r : rooms) {
+  for (Room* r : rooms) {
     r->setRoomOxygen(oxygen);
   }
   mapOxygen = oxygen;
@@ -58,7 +51,7 @@ void Map::calculateMapOxygen() {
   //TODO(mart2720): This function will need to
   //                change if we do oxygen propagation
   double temp = 0;
-  for(Room* r : rooms) {
+  for (Room* r : rooms) {
     temp += r->getRoomOxygen();
   }
   temp /= rooms.size();
--- test/TestMap.cpp
+++ test/TestMap.cpp
@@ -14,33 +14,15 @@ TEST(TestMap, getRoomTest) {
   Map* m = new Map();
   Room* r = new Room();
 
-  std::cout << "Past the construction" << std::endl;
-
   EXPECT_EQ(m->getRoom("Room"), nullptr);
-  std::cout << "Past the getRoom statement" << std::endl;
-
-  std::cout << "The size of the rooms vector before" << m->getRooms().size() << std::endl;
   m->addRoom(r);
-  std::cout << "The size of the rooms vector " << m->getRooms().size() << std::endl;
-  std::cout << "Past the pushback statement" << std::endl;
-  std::cout << m->getRoom("Room") << std::endl;
-  std::cout << "Past the weird test" << std::endl;
-
   EXPECT_EQ(m->getRoom("Room")->getName(), "Room");
-
-  std::cout << "Past the first tests" << std::endl;
 
   m->getRoom("Room")->setName("Master Chief's Man Cave");
   EXPECT_EQ(m->getRoom("Master Chief's Man Cave")->getName(),
   "Master Chief's Man Cave");
   EXPECT_EQ(m->getRoom("Room"), nullptr);
 
-  std::cout << "Past the second tests" << std::endl;
-
   delete m;
-  std::cout << "Past delete m" << std::endl;
-
   delete r;
-  std::cout << "Past delete r" << std::endl;
-
 }
