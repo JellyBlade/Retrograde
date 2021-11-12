6941ff7319c5f218064941b86e4e690f7f51fc3a
Tyrell Martens <tyrell.martens@uleth.ca>
2021-11-11 17:03:53 -0700

    Test

--- test/TestRoom.cpp
+++ test/TestRoom.cpp
@@ -22,19 +22,19 @@ TEST(TestRoom, constructorTest) {
   EXPECT_EQ(r1->getName().size(), 4);
   EXPECT_EQ(r1->getDescription(), "Empty room");
   EXPECT_EQ(r1->getDescription().size(), 10);
-  EXPECT_EQ(typeid(r1->getDoors()[Globals::Direction::NORTH]), typeid(Door));
-  EXPECT_EQ(typeid(r1->getDoors()[Globals::Direction::EAST]), typeid(Door));
-  EXPECT_EQ(typeid(r1->getDoors()[Globals::Direction::SOUTH]), typeid(Door));
-  EXPECT_EQ(typeid(r1->getDoors()[Globals::Direction::WEST]), typeid(Door));
+  EXPECT_EQ(typeid(*(r1->getDoors()[Globals::Direction::NORTH])), typeid(Door));
+  EXPECT_EQ(typeid(*(r1->getDoors()[Globals::Direction::EAST])), typeid(Door));
+  EXPECT_EQ(typeid(*(r1->getDoors()[Globals::Direction::SOUTH])), typeid(Door));
+  EXPECT_EQ(typeid(*(r1->getDoors()[Globals::Direction::WEST])), typeid(Door));
 
   EXPECT_EQ(r2->getName(), "Boiler Room");
   EXPECT_EQ(r2->getName().size(), 11);
   EXPECT_EQ(r2->getDescription(), "Casually boiling stuff");
   EXPECT_EQ(r2->getDescription().size(), 23);
-  EXPECT_EQ(typeid(r2->getDoors()[Globals::Direction::NORTH]), typeid(Door));
-  EXPECT_EQ(typeid(r2->getDoors()[Globals::Direction::EAST]), typeid(Door));
-  EXPECT_EQ(typeid(r2->getDoors()[Globals::Direction::SOUTH]), typeid(Door));
-  EXPECT_EQ(typeid(r2->getDoors()[Globals::Direction::WEST]), typeid(Door));
+  EXPECT_EQ(typeid(*(r2->getDoors()[Globals::Direction::NORTH])), typeid(Door));
+  EXPECT_EQ(typeid(*(r2->getDoors()[Globals::Direction::EAST])), typeid(Door));
+  EXPECT_EQ(typeid(*(r2->getDoors()[Globals::Direction::SOUTH])), typeid(Door));
+  EXPECT_EQ(typeid(*(r2->getDoors()[Globals::Direction::WEST])), typeid(Door));
 
   delete r1;
   delete r2;
