71456d181d89f89a22a6499ab50547bd3623f4ec
Tyrell Martens <tyrell.martens@uleth.ca>
2021-11-12 11:42:29 -0700

    Classic Cout tests because SEG FAULT😑

--- test/TestMap.cpp
+++ test/TestMap.cpp
@@ -14,15 +14,25 @@ TEST(TestMap, getRoomTest) {
   Map* m = new Map();
   Room* r = new Room();
 
+  std::cout << "Past the construction" << endl;
+
   EXPECT_EQ(m->getRoom("Room"), nullptr);
   m->getRooms().push_back(r);
   EXPECT_EQ(m->getRoom("Room")->getName(), "Room");
+
+  std::cout << "Past the first tests" << endl;
 
   m->getRoom("Room")->setName("Master Chief's Man Cave");
   EXPECT_EQ(m->getRoom("Master Chief's Man Cave")->getName(),
   "Master Chief's Man Cave");
   EXPECT_EQ(m->getRoom("Room"), nullptr);
 
+  std::cout << "Past the second tests" << endl;
+
   delete m;
-  delete r; 
+  std::cout << "Past delete m" << endl;
+
+  delete r;
+  std::cout << "Past delete r" << endl;
+
 }
