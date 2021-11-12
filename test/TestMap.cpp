c8ab336c4d33c649040d05e63130718f6b8a1b1d
Tyrell Martens <tyrell.martens@uleth.ca>
2021-11-12 11:43:48 -0700

    Forgot std:: on endl

--- test/TestMap.cpp
+++ test/TestMap.cpp
@@ -14,25 +14,25 @@ TEST(TestMap, getRoomTest) {
   Map* m = new Map();
   Room* r = new Room();
 
-  std::cout << "Past the construction" << endl;
+  std::cout << "Past the construction" << std::endl;
 
   EXPECT_EQ(m->getRoom("Room"), nullptr);
   m->getRooms().push_back(r);
   EXPECT_EQ(m->getRoom("Room")->getName(), "Room");
 
-  std::cout << "Past the first tests" << endl;
+  std::cout << "Past the first tests" << std::endl;
 
   m->getRoom("Room")->setName("Master Chief's Man Cave");
   EXPECT_EQ(m->getRoom("Master Chief's Man Cave")->getName(),
   "Master Chief's Man Cave");
   EXPECT_EQ(m->getRoom("Room"), nullptr);
 
-  std::cout << "Past the second tests" << endl;
+  std::cout << "Past the second tests" << std::endl;
 
   delete m;
-  std::cout << "Past delete m" << endl;
+  std::cout << "Past delete m" << std::endl;
 
   delete r;
-  std::cout << "Past delete r" << endl;
+  std::cout << "Past delete r" << std::endl;
 
 }
