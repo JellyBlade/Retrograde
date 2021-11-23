fad5b73f89a27aeb97a59061d29d41497b6d41d8
Tyrell Martens <tyrell.martens@uleth.ca>
2021-11-23 14:59:54 -0700

    Added implementation for Box, dont know how to add the object into the players inventory.

---
+++ src/Box.cpp
@@ -0,0 +1,34 @@
+/**
+* @author Tyrell M, Tyler H, Landon C, Truman L
+* @date 2021-11
+*/
+
+#include <iostream>
+#include "Box.h"
+
+using std::cout;
+using std::endl;
+
+void Box::interact() {
+  std::string item;
+  cout << "You look into the box and find";
+  for(int i = 0; i < objects.size(); i++) {
+    if (objects.size() == 0) {
+      cout << " nothing." << endl;
+      return;
+    }
+    cout << ":" << endl;
+    for (Object* o : objects.getObjects()) {
+      cout << " - " << o->getName() << endl;
+    }
+  }
+  cout << "Which item did you want to pick up?" << endl;
+  std::cin >> item;
+
+  if (objects.isObjectInContainer(item)) {
+    objects.removeObject(objects.getObject(item));
+    //TODO(mart2720): how do i add it to the specific player's inventory?
+  }
+  else
+    cout << "Could not find item." << endl;
+}
