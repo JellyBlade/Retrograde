34fcffa4b4861db528d0be9935d4ff8cce77c8e9
Tyler Hippard <tyler.hippard@uleth.ca>
2021-11-29 01:10:02 -0700

    More JSON stuff

---
+++ include/GenerateHelper.h
@@ -0,0 +1,55 @@
+/**
+* @author Tyrell M, Tyler H, Landon C, Truman L
+* @date 2021-11
+*/
+
+#ifndef GENERATE_HELPER_H
+#define GENERATE_HELPER_H
+
+#include <vector>
+#include <string>
+
+#include "nlohmann/json.hpp"
+#include "Object.h"
+#include "Room.h"
+#include "Map.h"
+
+using json = nlohmann::json;
+
+/**
+* Provides functions for instantiating objects/rooms from JSON.
+*/
+namespace GenerateHelper {
+
+  std::vector<json> jsonObjects;
+  std::vector<json> jsonRooms;
+
+  /**
+  * Populates the objects and rooms vectors for the other functions to use.
+  * Does not need to be called independently.
+  */
+  void setup();
+
+  /**
+  * Generates an Object from JSON based on the name.
+  * @param objectName The name of the object to generate.
+  * @return The JSON-generated object.
+  */
+  Object* generateObject(std::string objectName);
+
+  /**
+  * Generates a Room from JSON, with room objects if available.
+  * @param roomName the name of the room to generate.
+  * @return the JSON-generated room.
+  */
+  Room* generateRoom(std::string roomName);
+
+  /**
+  * Generates all of the rooms, doors, and objects for the map.
+  * @param mapName the map to generate, used for the file name.
+  */
+  Map* generateMap(std::string mapName);
+
+} // namespace GenerateHelper
+
+#endif // GENERATE_HELPER_H
---
+++ src/GenerateHelper.cpp
@@ -0,0 +1,96 @@
+/**
+* @author Tyrell M, Tyler H, Landon C, Truman L
+* @date 2021-11
+*/
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <stdexcept>
+
+#include "GenerateHelper.h"
+#include "TextHelper.h"
+#include "Door.h"
+#include "Object.h"
+#include "Log.h"
+#include "OxygenTank.h"
+#include "ItemLock.h"
+#include "PasswordLock.h"
+#include "Box.h"
+#include "AirLock.h"
+
+using json = nlohmann::json;
+
+void GenerateHelper::setup() {
+  std::ifstream file("text/objects.json");
+  if (!file.is_open()) {
+    throw std::runtime_error("objects.json not found!");
+  }
+  json j = json::parse(file);
+  for (auto it = j.begin(); it != j.end(); ++it) {
+    jsonObjects.push_back(it.value());
+  }
+  file.open("text/rooms.json");
+  if (!file.is_open()) {
+    throw std::runtime_error("rooms.json not found!");
+  }
+  j = json::parse(file);
+  for (auto it = j.begin(); it != j.end(); ++it) {
+    jsonRooms.push_back(it.value());
+  }
+}
+
+Object* GenerateHelper::generateObject(std::string objectName) {
+  if (jsonObjects.size() < 1) { setup(); }
+  objectName = TextHelper::tolower(TextHelper::trimAll(objectName));
+  for (auto o : jsonObjects) {
+    if (TextHelper::tolower(TextHelper::trimAll(o["name"])) == objectName) {
+      if (o.contains("box")) {
+        Box* box = new Box(o["name"], o["desc"], o["holdable"]);
+        for (auto& boxObj : o["box"]) {
+          box->addObject(generateObject(boxObj.get<std::string>()));
+        }
+        return box;
+      } else if (o.contains("password")) {
+        //return new PasswordLock(o["name"], o["desc"], o["holdable"],
+        o["password"]);
+      } else if (o.contains("oxygenTankReuse")) {
+        //return new OxygenTank(o["name"], o["desc"], o["holdable"],
+        //o["oxygenTankReuse"]);
+      } else if (o.contains("reqItem")) {
+        //return new ItemLock(o["name"], o["desc"], o["holdable"], o["reqItem"]);
+      } else if (o.contains("reqOxygen")) {
+        //return new AirLock(o["name"], o["desc"], o["holdable"], o["reqOxygen"]);
+      } else if (o.contains("logFile")) {
+        //return new Log(o["name"], o["desc"], o["holdable"], o["logFile"]);
+      } else {
+        return new Object(o["name"], o["desc"], o["holdable"]);
+      }
+    }
+  }
+}
+
+Room* GenerateHelper::generateRoom(std::string roomName) {
+  if (jsonRooms.size() < 1) { setup(); }
+  roomName = TextHelper::tolower(TextHelper::trimAll(roomName));
+  for (auto r : jsonRooms) {
+    if (TextHelper::tolower(TextHelper::trimAll(r["name"])) == roomName) {
+      Room* room = new Room(r["name"], r["desc"]);
+      room->setRoomOxygen(r["oxygen"]);
+      for (auto& roomObject : r["objects"]) {
+        room->getRoomObjects()->addObject(generateObject(roomObject.get<std::string>()));
+      }
+      return room;
+    }
+  }
+}
+
+Map* GenerateHelper::generateMap(std::string mapName) {
+  if (jsonObjects.size() < 1 || jsonRooms.size() < 1) { setup(); }
+  mapName = TextHelper::tolower(TextHelper::trimAll(mapName));
+  std::ifstream file("text/" + mapName + ".json");
+  if (!file.is_open()) {
+    throw std::runtime_error(mapName + ".json not found!");
+  }
+  json mapJSON = json::parse(file);
+}
