03f567aa3da4cfb1c23042cadb51c9ae9f4d8696
Tyrell Martens <tyrell.martens@uleth.ca>
2021-12-01 06:56:10 -0700

    Some fixes, for some reason compile Project doesnt show errors

--- test/TestLockedBox.cpp
+++ test/TestLockedBox.cpp
@@ -11,6 +11,7 @@
 #include "Game.h"
 #include "InteractHelper.h"
 #include "LockedBox.h"
+#include "PasswordLock.h"
 #include "gtest/gtest.h"
 
 TEST(LockedBoxTest, ConstructorTest) {
@@ -18,7 +19,7 @@ TEST(LockedBoxTest, ConstructorTest) {
   Object* o1 = new Object("test2", "test2", false);
   Object* o2 = new Object("You're a Harry Wizard", "yer a wezrd hry", false);
   Puzzle* p = new PassworkLock();
-  LockedBox* b1 = new LockedBox();
+  LockedBox* b = new LockedBox();
   p->setSolved();
   LockedBox* b2 = new LockedBox("Treasure Chest", "Gold inside", true, p);
   b->addObject(o);
@@ -29,11 +30,16 @@ TEST(LockedBoxTest, ConstructorTest) {
   EXPECT_TRUE(b->getBoxObjects()->isObjectInContainer(o1));
   EXPECT_FALSE(b->getBoxObjects()->isObjectInContainer(o2));
 
+  EXPECT_EQ(b2->getBoxObjects()->size(), 0);
+  EXPECT_FALSE(b2->getBoxObjects()->isObjectInContainer(o));
+  EXPECT_FALSE(b2->getBoxObjects()->isObjectInContainer(o1));
+  EXPECT_FALSE(b2->getBoxObjects()->isObjectInContainer(o2));
+
   delete b1;
   delete b2;
-  std::cout << "Probably going to be a seg fault here" << endl;
+  std::cout << "Probably going to be a seg fault here" << std::endl;
   delete p;
-  std::cout << "Yay" << endl;
+  std::cout << "Yay" << std::endl;
   delete o2;
-  std::cout << "May happen after function." << endl;
+  std::cout << "May happen after function." << std::endl;
 }
