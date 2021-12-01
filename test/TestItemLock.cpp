b3bc64d0be75ea94c66deee65722753ba96c3734
Tyrell Martens <tyrell.martens@uleth.ca>
2021-12-01 14:56:44 -0700

    Testing of ItemLock

--- test/TestItemLock.cpp
+++ test/TestItemLock.cpp
@@ -2,7 +2,24 @@
 * @author Tyrell M, Tyler H, Landon C, Truman L
 * @date 2021-11
 */
-/*
+
+#include "InteractHelper.h"
+#include "ItemLock.h"
+#include "gtest/gtest.h"
+
 TEST(TestItemLock, interactTest) {
-  will implement later
-}*/
+  Game* game = new Game();
+  InteractHelper::game = game;
+  Object* o = new Object("Key", "unlocks someting", false);
+  ItemLock* i = new ItemLock("Locker", "Something is inside", false, "Key");
+
+  EXPECT_EQ(i->isPuzzleSolved(), false);
+  i->interact();
+  EXPECT_EQ(i->isPuzzleSolved(), false);
+  InteractHelper::getPlayerInventory()->addObject(o);
+  i->interact();
+  EXPECT_EQ(i->isPuzzleSolved(), true);
+
+  delete game;
+  delete i;
+}
