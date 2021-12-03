241a06347629849c672660dff1b392c67fd65393
Tyler Hippard <tyler.hippard@uleth.ca>
2021-12-02 18:22:11 -0700

    Adjusted tests (File was possible closed?)

--- test/TestPasswordLock.cpp
+++ test/TestPasswordLock.cpp
@@ -14,20 +14,22 @@ TEST(TestPasswordLock, solveTest) {
   PasswordLock* p = new PasswordLock("test");
   PasswordLock* p1 = new PasswordLock("IPhone", "Requires password to unlock",
 false, "9863HiTheRe");
-  std::ifstream file("test/text/passwordLockTest.txt");
-
+  std::ifstream file("test/text/passwordLockTest1.txt");
 
   EXPECT_EQ(p->isPuzzleSolved(), false);
   EXPECT_FALSE(p->playerInput(file));
   EXPECT_EQ(p->isPuzzleSolved(), false);
   EXPECT_TRUE(p->playerInput(file));
   EXPECT_EQ(p->isPuzzleSolved(), true);
+  file.close()
 
+  file.open("test/text/passwordLockTest2.txt")
   EXPECT_EQ(p1->isPuzzleSolved(), false);
   EXPECT_FALSE(p1->playerInput(file));
   EXPECT_EQ(p1->isPuzzleSolved(), false);
   EXPECT_TRUE(p1->playerInput(file));
   EXPECT_EQ(p1->isPuzzleSolved(), true);
+  file.close();
 
   delete p;
   delete p1;
